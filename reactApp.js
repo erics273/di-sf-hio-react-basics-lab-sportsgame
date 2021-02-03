941049038c7c0436a40a918603dce3c7fd928f10
Svei <avn.jan5@gmail.com>
2021-02-02 21:40:11 -0600

    finished

--- reactApp.js
+++ reactApp.js
@@ -1,11 +1,127 @@
+class Team extends React.Component {
+  constructor(props) {
+    super(props)
 
+    this.state = {
+      shots: 0,
+      score: 0
+    
+    }
+
+    this.shotSound = new Audio('./assets/audio/smb_fireball.wav')
+    this.scoreSound = new Audio('./assets/audio/smb_1-up.wav')
+  }
+
+  shotHandler = () => {
+    let score = this.state.score
+    this.shotSound.play()
+
+    if (Math.random() > 0.5) {
+      score += 1
+      
+      setTimeout(() => {
+        this.scoreSound.play()
+      }, 150)
+      
+    }
+
+    this.setState((state, props) => ({
+      shots: state.shots + 1,
+      score 
+    }))
+  }
+  render() {
+    
+    let shotPercentageDiv
+
+    if (this.state.shots) {
+      let shotPercentage = Math.round((this.state.score / this.state.shots) * 100)
+      shotPercentageDiv = (
+        <div>
+          <strong>Shooting %: {shotPercentage} </strong>
+        </div>
+      )
+    }
+
+    return (
+      <div className="Team">
+        <h2>{this.props.name}</h2>
+        <div className="identity">
+          <img src={this.props.logo} alt={this.props.name} />
+        </div>
+
+        <div>
+          <strong>Shots:</strong> {this.state.shots}
+        </div>
+
+        <div>
+          <strong>Score:</strong> {this.state.score}
+        </div>
+
+        {shotPercentageDiv}
+        
+        <button onClick={this.shotHandler}>Shoot!</button>
+      </div>
+    )
+  }
+}
+
+function Game(props) {
+  return (
+    <div className="Game">
+      <h1>Welcome to {props.venue}</h1>
+      <div className="stats">
+     <div>
+      <Team 
+      name={props.visitingTeam.name}
+      logo={props.visitingTeam.logoSrc}
+      />
+
+      <div className="verses">
+        <h1>VS</h1>
+      </div>
+
+      <Team 
+      name={props.homeTeam.name} 
+      logo={props.homeTeam.logoSrc}
+      />
+    </div>
+    </div>
+    </div>
+  )
+}
 // Deafault App component that all other compents are rendered through
 function App(props){
+  const raccoons = {
+    name: 'Russiaville Raccoons',
+    logoSrc: './assets/images/raccoon.png'
+  }
+  const squirrels = {
+    name: 'Sheridian Squirrels',
+    logoSrc: './assets/images/squirrel.png'
+  }
+  const bunnies = {
+    name: 'Burlington Bunnies',
+    logoSrc: './assets/images/bunny.png'
+  }
+  const hounds = {
+    name: 'Hammond Hounds',
+    logoSrc: './assets/images/hound.png'
+  }
+
   return (
-    <div>
-      <h1>Welcome to the sports game starter</h1>
-      This file represents the code after completing the setup step in the lab instructions
-    </div>
+    <div className="App">
+      <Game 
+      venue="Union 525 Gem" 
+      homeTeam={squirrels}
+      visitingTeam={raccoons}
+      />
+      <Game 
+      venue="Burlington Arena" 
+      homeTeam={bunnies}
+      visitingTeam={hounds}
+      />
+  </div>
   )
 }
 
